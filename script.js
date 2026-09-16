7cfa4b2918e1458748e1f3273fc195a7a599be18
HIBA53757 <hibatallahsadiki20@gmail.com>
2026-09-16 17:23:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+
+let products = []
+async function loadProducts() {
+
+    try {
+        const reponse = await fetch("/data/products.json")
+        const data = await reponse.json()
+        products = data;
+        console.log(products);
+
+
+        displayProducts(products);
+    } catch (error) {
+        console.error(error)
+    }
+}
+loadProducts()
+
+const productsload = document.querySelector("#products");
+const displaycategory = document.querySelector("#category");
+
+function displayProducts(products) {
+
+
+    const productsMapped = products.map(product => {
+        return {
+            name: product.name,
+            category: product.category,
+            price: product.price,
+            stock: product.stock
+        }
+    })
+    productsload.innerHTML = "";
+    productsMapped.forEach(product => {
+        productsload.innerHTML += `
+            <article>
+                <h3>${product.name}</h3>
+                <p>price : ${product.price} $</p>
+                <p>category: ${product.category}</p>
+               <p>stock: ${product.stock}</p>
+
+            </article>
+        `
+    });
+
+
+    displaycategory.innerHTML = "";
+    productsMapped.forEach(product => {
+        displaycategory.innerHTML += `
+    <option>category: ${product.category}</option>
+ `
+    });
+}
+function filterCategory() {
+    const filter = products.filter(product => products.category)
+
+}
+// displaycategory.addEventListener("click", filterCategory(){
+
+// })
